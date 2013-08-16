8e9e108ff5d82e492648c1c94d3bd94ad166b174
jona <omnigoat@gmail.com>
2013-08-16 18:35:17 +1000

    fooey

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include <shiny/voodoo/command.hpp>
 #include <shiny/plumbing/vertex_buffer.hpp>
 
+#include <fooey/fooey.hpp>
 
 int main()
 {
@@ -15,8 +16,8 @@ int main()
 	SR.add_context_thread([] {
 		float f[] = { 1, 2, 3, 4 };
 		shiny::plumbing::vertex_buffer_t vb(shiny::voodoo::gpu_access_t::read, shiny::voodoo::cpu_access_t::write, true, 16, f);
-		//shiny::plumbing::locked_vertex_buffer_t lvb(vb, shiny::plumbing::lock_type_t::write_discard);
-		//auto i = lvb.begin<float>();
-		//std::copy(f, f + 4, i);
 	});
+
+	fooey::window_t W("blam");
+
 }
