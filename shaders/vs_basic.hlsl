cdd3817622ad446107d5e582d4ff29f5d1941020
jona <omnigoat@gmail.com>
2013-08-19 09:21:39 +1000

    updates for fooey

--- shaders/vs_basic.hlsl
+++ shaders/vs_basic.hlsl
@@ -1,8 +1,9 @@
-cbuffer {
+cbuffer vert_in
+{
     float4x4 wvp_matrix;
 }
 
-float4 main(float4 position)
+float4 main(float4 position : POSITION) : SV_POSITION
 {
-	return wvp_matrix * position;
+	return mul(wvp_matrix, position);
 }
--- source/main.cpp
+++ source/main.cpp
@@ -7,17 +7,46 @@
 #include <shiny/voodoo/command.hpp>
 #include <shiny/plumbing/vertex_buffer.hpp>
 
-#include <fooey/fooey.hpp>
+#include <fooey/widgets/window.hpp>
+#include <atma/evented/event.hpp>
+
+void blah() {
+	std::cout << "blah" << std::endl;
+}
 
 int main()
 {
 	shiny::scoped_runtime_t SR;
+	
 	
 	SR.add_context_thread([] {
 		float f[] = { 1, 2, 3, 4 };
 		shiny::plumbing::vertex_buffer_t vb(shiny::voodoo::gpu_access_t::read, shiny::voodoo::cpu_access_t::write, true, 16, f);
 	});
 
-	fooey::window_t W("blam");
 
+	// setup up gui
+	fooey::window_ptr wnd = fooey::window("blam");
+	//fooey::map_basic_window_events(wnd);
+	wnd->on_minimise += blah;
+	wnd->on_minimise.fire();
+	#if 0
+	
+	
+	//fooey::map_close_to_shutdown(wnd);
+
+	wnd->window_events.on_minimise += [](fooey::window_ptr const& x, et const&) {
+		x->set_window_state(fooey::)
+	};
+	
+	fooey::map_window_events(wnd,
+	{
+		{fooey::window_event_t::minimise, [](wp const& x, et const&) {
+			fooey::set_window_state(x, fooey::window_t::minimised);
+		}},
+		{fooey::window_event_t::maximise, [](wp const& x, et const&) {
+			fooey::set_window_state(x, fooey::window_t::maximised);
+		}}
+	});
+	#endif
 }
