7401e2111351cca6b033393f1143cb8b4b66a984
jona <omnigoat@gmail.com>
2013-08-21 01:43:45 +1000

    showcase of bind

--- source/main.cpp
+++ source/main.cpp
@@ -11,12 +11,49 @@
 #include <fooey/fooey.hpp>
 #include <atma/evented/event.hpp>
 
-void blah() {
-	std::cout << "blah" << std::endl;
+void print_4(int a, int b, int c, int d) {
+	std::cout << "blah: " << a << ", " << b << ", " << c << ", " << d << std::endl;
 }
+
+struct printer_t
+{
+	printer_t() : x(-1) {}
+	printer_t(int x) : x(x) {}
+
+	void print_4_x(int a, int b, int c, int d)
+	{
+		std::cout << "blah: " << a << ", " << b << ", " << c << ", " << d << ", " << x << std::endl;
+	}
+
+	int x;
+};
+
+static atma::xtm::detail::xtm_ph<0> _1;
+static atma::xtm::detail::xtm_ph<1> _2;
+static atma::xtm::detail::xtm_ph<2> _3;
+
 
 int main()
 {
+	// function pointer
+	auto b = atma::xtm::bind(&print_4, 1, 2, _2, _1);
+	b(4, 3);
+	
+	// member function pointer
+	printer_t printer(5);
+	auto b2 = atma::xtm::bind(&printer_t::print_4_x, _3, 1, _2, 3, _1);
+	b2(4, 2, &printer);
+
+	// lambda
+	auto L = [](int a, int b) -> int {return a + b; };
+	auto b3 = atma::xtm::bind(L, 7, _1);
+	std::cout << b3(3) << std::endl;
+	
+	
+	
+
+	return 0;
+
 	auto SR = shiny::scoped_runtime_t{};
 	
 	
