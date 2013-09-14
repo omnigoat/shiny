c17cef13185aad1236084114989e42dcd171cb34
jona <omnigoat@gmail.com>
2013-09-14 21:58:37 +1000

    added device for per-window contexts

--- include/shiny/voodoo/device.hpp
+++ include/shiny/voodoo/device.hpp
@@ -42,6 +42,21 @@ namespace voodoo {
 	auto setup_d3d_device() -> void;
 	auto teardown_d3d_device() -> void;
 	
+	enum class display_format_t
+	{
+		unknown,
+		r32g32b32a32 = 1,
+		r32g32b32a32_f32 = 2,
+		r8g8b8a8_unorm = 28
+	};
+
+	struct display_mode_t
+	{
+		uint32_t width, height;
+		uint32_t refreshrate_frames, refreshrate_period;
+		display_format_t format;
+	};
+
 	struct context_t : atma::ref_counted
 	{
 		context_t(fooey::window_ptr const&, uint32_t width, uint32_t height);
@@ -50,9 +65,21 @@ namespace voodoo {
 		auto toggle_fullscreen() -> void;
 
 	private:
+		auto enumerate_backbuffers() -> void;
+		auto closest_display_mode(uint32_t width, uint32_t height) -> display_mode_t;
+		auto create_swapchain() -> void;
+
+		IDXGIDevice1* dxgi_device_;
+		IDXGIAdapter1* dxgi_adapter_;
+		IDXGIFactory1* dxgi_factor_;
+		IDXGISwapChain* dxgi_swap_chain_;
+
 		fooey::window_ptr window_;
 		uint32_t width_, height_;
-		IDXGISwapChain* dxgi_swap_chain_;
+		
+
+		typedef std::vector<display_mode_t> display_modes_t;
+		display_modes_t backbuffer_display_modes_;
 
 		uint32_t on_resize_handle_;
 
--- source/voodoo/device.cpp
+++ source/voodoo/device.cpp
@@ -70,33 +70,10 @@ context_t::context_t(fooey::window_ptr const& window, uint32_t width, uint32_t h
 	if (height_ == 0)
 		height_ = window_->height_in_pixels();
 
-	IDXGIDevice1* dxgi_device = nullptr;
-	ATMA_ENSURE_IS(S_OK, detail::d3d_device_->QueryInterface(__uuidof(IDXGIDevice1), (void**)&dxgi_device));
+	enummerate_backbuffers();
+	create_swapchain();
 
-	IDXGIAdapter1* dxgi_adapter = nullptr;
-	ATMA_ENSURE_IS(S_OK, dxgi_device->GetParent(__uuidof(IDXGIAdapter1), (void**)&dxgi_adapter));
-
-	IDXGIFactory1* dxgi_factory = nullptr;
-	ATMA_ENSURE_IS(S_OK, dxgi_adapter->GetParent(__uuidof(IDXGIFactory1), (void**)&dxgi_factory));
-
-	auto desc = DXGI_SWAP_CHAIN_DESC{
-		// DXGI_MODE_DESC
-		{width_, height_, { 0, 0 }, DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE, DXGI_MODE_SCALING_UNSPECIFIED},
-		// DXGI_SAMPLE_DESC
-		{ 1, 0 },
-		0,
-		3,
-		window->hwnd,
-		TRUE,
-		DXGI_SWAP_EFFECT_DISCARD,
-		0
-	};
-
-	IDXGISwapChain* dxgi_swap_chain = nullptr;
-	ATMA_ENSURE_IS(S_OK, dxgi_factory->CreateSwapChain(detail::d3d_device_, &desc, &dxgi_swap_chain));
-
-
-	on_resize_handle_ = window_->on_resize.connect([dxgi_swap_chain](atma::event_flow_t&, uint32_t width, uint32_t height) {
+	on_resize_handle_ = window_->on_resize.connect([&](atma::event_flow_t&, uint32_t width, uint32_t height) {
 		ATMA_ENSURE_IS(S_OK, dxgi_swap_chain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0));
 	});
 }
@@ -105,6 +82,33 @@ context_t::~context_t()
 {
 	window_->on_resize.disconnect(on_resize_handle_);
 }
+
+auto context_t::enumerate_backbuffers() -> void
+{
+	
+}
+
+auto context_t::create_swapchain() -> void
+{
+	ATMA_ENSURE_IS(S_OK, detail::d3d_device_->QueryInterface(__uuidof(IDXGIDevice1), (void**)&dxgi_device_));
+	ATMA_ENSURE_IS(S_OK, dxgi_device->GetParent(__uuidof(IDXGIAdapter1), (void**)&dxgi_adapter_));
+	ATMA_ENSURE_IS(S_OK, dxgi_adapter->GetParent(__uuidof(IDXGIFactory1), (void**)&dxgi_factory_));
+
+	auto format = closest_display_mode(width_, height_);
+
+	
+	auto desc = DXGI_SWAP_CHAIN_DESC{
+		// DXGI_MODE_DESC
+		{format.width, format.height, {format.refreshrate_frames, format.refreshrate_period}, DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE, DXGI_MODE_SCALING_UNSPECIFIED},
+		// DXGI_SAMPLE_DESC
+		{1, 0},
+		0, 3, window->hwnd, TRUE, DXGI_SWAP_EFFECT_DISCARD, 0
+	};
+
+	ATMA_ENSURE_IS(S_OK, dxgi_factory->CreateSwapChain(detail::d3d_device_, &desc, &dxgi_swap_chain_));
+}
+
+
 
 auto context_t::toggle_fullscreen() -> void
 {
