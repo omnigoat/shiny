0c9c7f14c5644b108c8855a8281617ead2139d22
jona <omnigoat@gmail.com>
2014-06-14 17:18:43 +1000

    moved generic_buffer constructor to source-file

---
+++ source/dust/platform/win32/generic_buffer.cpp
@@ -0,0 +1,19 @@
+#include <dust/platform/win32/generic_buffer.hpp>
+
+#include <dust/context.hpp>
+
+using namespace dust;
+using dust::generic_buffer_t;
+
+
+generic_buffer_t::generic_buffer_t(context_ptr const& ctx, buffer_usage_t usage, surface_format_t format, uint elements, void const* data, uint data_elemcount)
+: buffer_t(ctx, buffer_type_t::generic_buffer, usage, platform::dxgi_size_of(format), elements, data, data_elemcount),
+  element_count_(elements)
+{
+	auto desc = D3D11_SHADER_RESOURCE_VIEW_DESC{};
+	desc.Format = DXGI_FORMAT_UNKNOWN;
+	desc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+	desc.Buffer = {0, data_elemcount};
+
+	ATMA_ENSURE_IS(S_OK, context()->d3d_device()->CreateShaderResourceView(d3d_resource().get(), &desc, d3d_srv_.assign()));
+}
