ed34f0c2014423a7484520be675bf4e2e8df0451
unknown <jrunting@APMELDPLY028.eamobile.ad.ea.com>
2015-01-23 11:16:03 +1100

    testing out a new event_t

--- source/shoxelizer/shoxelizer.cpp
+++ source/shoxelizer/shoxelizer.cpp
@@ -10,9 +10,6 @@
 
 
 
-
-
-
 namespace shelf
 {
 	enum class path_type_t
@@ -338,9 +335,303 @@ auto octree_t::node_t::oct_subbound(math::vector4f const& bounds, uint idx) -> m
 
 
 
+struct bb_y
+{
+	template <typename T>
+	auto firde(T t) -> void
+	{
+		dynamic_cast<base_thing_t<T>*>(this)->fire_impl(t);
+	}
+
+	virtual ~bb_y() {}
+};
+
+template <typename T>
+struct base_thing_t : virtual bb_y
+{
+	auto fire_impl(T t) -> void {}
+};
+
+enum class things_t { lol, hooray };
+enum class animals_t { giraffe, dragon };
+
+
+struct sizing_events_t
+{
+	int resize;
+	int resize_dc;
+	int move;
+};
+
+enum class sizing_event_type_t
+{
+	resize,
+	resize_dc,
+	move,
+};
+
+
+#include <atma/event.hpp>
+#include <functional>
+#include <list>
+#include <set>
+#include <mutex>
+
+
+template <typename T>
+struct null_combiner_t
+{
+	null_combiner_t()
+		: result_()
+	{}
+
+	using result_type = T;
+
+	auto reset() -> void { result_ = T(); }
+	auto push(T const& x) -> void { result_ += x; }
+	auto result() -> T { return result_; }
+
+private:
+	T result_;
+};
+
+template <>
+struct null_combiner_t<void>
+{
+	null_combiner_t()
+	{}
+
+	using result_type = void;
+
+	auto reset() -> void {}
+	auto push(void) -> void { }
+	auto result() const -> void { return; }
+};
+
+template <typename T>
+struct count_combiner_t
+{
+	count_combiner_t()
+		: calls_()
+	{}
+
+	using result_type = uint;
+
+	auto reset() -> void { calls_ = 0u; }
+	template <typename Y> auto push(Y const&) -> void { ++calls_; }
+	auto push(void) -> void { ++calls_; }
+	auto result() -> uint { return calls_; }
+
+private:
+	uint calls_;
+};
+
+namespace detail
+{
+	template <typename FN>
+	using delegate_t = std::function<FN>;
+
+	template <typename FN>
+	using delegates_t = std::list<delegate_t<FN>>;
+
+
+
+	template <typename R, template <typename> class CMB, typename FN, typename... Args>
+	struct fire_impl_t
+	{
+		static auto go(CMB<R>& cmb, delegates_t<FN> const& delegates, Args&&... args) -> typename CMB<R>::result_type
+		{
+			cmb.reset();
+
+			for (auto const& x : delegates)
+				cmb.push(x(std::forward<Args>(args)...));
+
+			return cmb.result();
+		}
+	};
+	
+
+	template <template <typename> class CMB, typename FN, typename... Args>
+	struct fire_impl_t<void, CMB, FN, Args...>
+	{
+		static auto go(CMB<void>& cmb, delegates_t<FN> const& delegates, Args&&... args) -> typename CMB<void>::result_type
+		{
+			cmb.reset();
+
+			for (auto const& x : delegates) {
+				x(std::forward<Args>(args)...);
+				cmb.push();
+			}
+
+			return cmb.result();
+		}
+	};
+
+}
+
+template <bool Use> struct maybe_mutex_t;
+template <bool Use> struct maybe_scoped_lock_t;
+
+template <>
+struct maybe_mutex_t<true>
+{
+	maybe_mutex_t() {}
+	maybe_mutex_t(maybe_mutex_t const&) = delete;
+
+private:
+	std::mutex mutex;
+
+	friend struct maybe_scoped_lock_t<true>;
+};
+
+template <>
+struct maybe_mutex_t<false>
+{
+	maybe_mutex_t(maybe_mutex_t const&) = delete;
+};
+
+template <>
+struct maybe_scoped_lock_t<true>
+{
+	maybe_scoped_lock_t(maybe_mutex_t<true>& mtx)
+		: guard_(mtx.mutex)
+	{}
+
+private:
+	std::lock_guard<std::mutex> guard_;
+};
+
+template <>
+struct maybe_scoped_lock_t<false>
+{
+	maybe_scoped_lock_t(maybe_mutex_t<false> const& mtx)
+	{}
+};
+
+
+template <typename FN, template <typename> class CMB = count_combiner_t, bool ThreadSafe = true>
+struct event_t
+{
+	using fnptr_type = std::decay_t<FN>;
+	using fn_result_type = typename atma::xtm::function_traits<fnptr_type>::result_type;
+
+	using delegate_t = std::function<FN>;
+	using delegates_t = detail::delegates_t<FN>;
+	using delegate_handle_t = typename delegates_t::const_iterator;
+
+	using combiner_t = CMB<fn_result_type>;
+	using result_type = typename combiner_t::result_type;
+
+	
+	event_t()
+	{}
+
+	event_t(combiner_t const& cmb)
+		: combiner_{cmb}
+	{}
+
+	event_t(event_t const& rhs)
+		: combiner_{rhs.combiner_}, mutex_{}, delegates_(rhs.delegates_)
+	{}
+
+	template <typename... Args>
+	auto fire(Args&&... args) -> result_type
+	{
+		auto SL = maybe_scoped_lock_t<ThreadSafe>{mutex_};
+
+		return detail::fire_impl_t<fn_result_type, CMB, FN, Args...>::go(combiner_, delegates_, std::forward<Args>(args)...);
+	}
+
+	auto operator += (delegate_t const& fn) -> delegate_handle_t
+	{
+		auto SL = maybe_scoped_lock_t<ThreadSafe>{mutex_};
+
+		delegates_.push_back(fn);
+		return --delegates_.end();
+	}
+
+	auto operator -= (delegate_handle_t const& fn) -> void
+	{
+		auto SL = maybe_scoped_lock_t<ThreadSafe>{mutex_};
+
+		delegates_.erase(fn);
+	}
+
+private:
+	
+
+private:
+	combiner_t combiner_;
+	delegates_t delegates_;
+	maybe_mutex_t<ThreadSafe> mutex_;
+};
+
+
+template <typename Range, typename C, typename M, typename T>
+struct propper_t;
+
+template <typename Range, typename C, typename M, typename... Args>
+struct propper_t<Range, C, M, std::tuple<Args...>>
+{
+	static auto go(Range&& range, M C::*member, Args... args) -> typename M::result_type
+	{
+		for (auto&& x : range)
+			(x.*member).fire(std::forward<Args>(args)...);
+
+		return typename M::result_type();
+	}
+};
+
+template <typename Range, typename C, typename M>
+auto broadcast_across(Range&& range, M C::*member) 
+-> typename M::delegate_t
+{
+	using params = typename atma::xtm::function_traits<typename M::fnptr_type>::tupled_params_type;
+
+	return atma::xtm::curry(&propper_t<Range, C, M, params>::go, std::ref(range), member);
+}
+
+
+
+
+
+struct dragon_t
+{
+	dragon_t() {}
+	
+	event_t<int(int)> on_breathe_fire;
+	
+	std::vector<dragon_t> children_;
+};
 
 int main()
 {
+	event_t<int(int, int), null_combiner_t> e;
+
+	e += [](int x, int y) -> int { return x + y; };
+	e += [](int x, int y) -> int { return x * y; };
+	auto r = e.fire(4, 5);
+
+	event_t<void(int, int)> e2;
+	int z = 0;
+	e += [&](int x, int y) { return z = x + y; };
+
+
+
+	{
+		auto d = dragon_t{};
+		d.children_.push_back(dragon_t());
+		d.children_.back().on_breathe_fire += [](int g) -> int { std::cout << g << std::endl; return 0; };
+		d.children_.back().on_breathe_fire += [](int g) -> int { std::cout << "lulz" << std::endl; return 0; };
+		d.children_.back().on_breathe_fire += [](int g) -> int { std::cout << "zomg" << std::endl; return 0; };
+		d.children_.back().on_breathe_fire += [](int g) -> int { std::cout << "stop setting things on fire" << std::endl; return 0; };
+		d.children_.back().on_breathe_fire.fire(88);
+		d.on_breathe_fire += broadcast_across(d.children_, &dragon_t::on_breathe_fire);
+		d.on_breathe_fire.fire(8);
+	}
+
+
+
 	auto numbers = std::vector<int>{1, 2, 3, 4};
 	auto is_even = [](int x) { return x % 2 == 0; };
 	auto filtered = atma::filter(is_even, numbers);
