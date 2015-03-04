bb867e92e50de7184d060d0b83e4b78d9edb3342
omnigoat <omnigoat@gmail.com>
2015-03-04 14:31:05 +1100

    shiny now a library, shoxelizer showing cube

--- source/shoxelizer/shoxelizer.cpp
+++ source/shoxelizer/shoxelizer.cpp
@@ -1,20 +1,42 @@
-#include <atma/string.hpp>
-#include <atma/intrusive_ptr.hpp>
-#include <atma/math/vector4f.hpp>
-#include <atma/algorithm.hpp>
-#include <atma/enable_if.hpp>
-#include <atma/math/intersection.hpp>
-#include <atma/bind.hpp>
-#include <atma/math/vector4i.hpp>
-#include <atma/filesystem/file.hpp>
-
-#include <filesystem>
-
 #include <dust/runtime.hpp>
 #include <dust/context.hpp>
+#include <dust/vertex_buffer.hpp>
+#include <dust/vertex_declaration.hpp>
+#include <dust/vertex_shader.hpp>
+#include <dust/fragment_shader.hpp>
+#include <dust/constant_buffer.hpp>
+#include <dust/index_buffer.hpp>
+#include <dust/camera.hpp>
+#include <dust/scene.hpp>
+#include <dust/texture2d.hpp>
+#include <dust/compute_shader.hpp>
+#include <dust/shader_resource2d.hpp>
+#include <dust/texture3d.hpp>
+#include <dust/platform/win32/generic_buffer.hpp>
 #include <shelf/file.hpp>
-#include <sstream>
 
+#include <fooey/widgets/window.hpp>
+#include <fooey/fooey.hpp>
+#include <fooey/events/resize.hpp>
+#include <fooey/events/mouse.hpp>
+#include <fooey/event_handler.hpp>
+#include <fooey/keys.hpp>
+
+#include <atma/math/matrix4f.hpp>
+#include <atma/math/vector4f.hpp>
+#include <atma/math/vector4i.hpp>
+#include <atma/math/intersection.hpp>
+
+#include <atma/string.hpp>
+#include <atma/intrusive_ptr.hpp>
+#include <atma/algorithm.hpp>
+#include <atma/enable_if.hpp>
+#include <atma/bind.hpp>
+#include <atma/filesystem/file.hpp>
+
+
+
+namespace math = atma::math;
 
 
 namespace shelf
@@ -327,7 +349,6 @@ auto octree_t::node_t::insert(math::triangle_t const& tri) -> bool
 		return false;
 	}
 
-	
 
 	data_.push_back(tri);
 
@@ -390,7 +411,8 @@ struct mesh_t
 	mesh_t(size_t vertices, size_t indices);
 
 private:
-	
+	vertices_t vertices_;
+	indices_t indices_;
 };
 
 obj_model_t::obj_model_t(shelf::file_t& file)
@@ -419,8 +441,195 @@ obj_model_t::obj_model_t(shelf::file_t& file)
 
 int main()
 {
+	auto tri = math::triangle_t {math::point4f(0.35f, 0.1f, 0.1f), math::point4f(0.85f, 0.2f, 0.3f), math::point4f(0.15f, 0.2f, 0.44f)};
+
+	auto r = math::intersect_aabc_triangle2(math::aabc_t{0.125f, 0.125f, 0.375f, 0.25f}, tri);
+
 	auto oct = octree_t{octree_allocate_tag{3}};
-	oct.insert(math::triangle_t{math::point4f(0.1f, 0.1f, 0.1f), math::point4f(0.6f, 0.2f, 0.3f), math::point4f(-0.10f, 0.2f, 0.44f)});
+	oct.insert(tri);
+
+
+	// draw debug cubes
+		// setup gui
+	auto renderer = fooey::system_renderer();
+	auto window = fooey::window("Excitement.", 480, 360);
+	renderer->add_window(window);
+
+	// initialise dust
+	auto dust_runtime = dust::runtime_t();
+	auto ctx = dust::create_context(dust_runtime, window, dust::primary_adapter);
+
+	// vertex declaration
+	auto vd = dust_runtime.vertex_declaration_of({
+		{dust::vertex_stream_semantic_t::position, 0, dust::element_format_t::f32x4},
+		{dust::vertex_stream_semantic_t::color, 0, dust::element_format_t::f32x4}
+	});
+
+	// shaders
+	auto f = atma::filesystem::file_t("../../shaders/vs_basic.hlsl");
+	auto fm = f.read_into_memory();
+	auto vs = dust::create_vertex_shader(ctx, vd, fm, false);
+
+	auto f2 = atma::filesystem::file_t("../../shaders/ps_basic.hlsl");
+	auto fm2 = f2.read_into_memory();
+	auto ps = dust::create_fragment_shader(ctx, fm2, false);
+
+	// vertex-buffer
+	float vbd[] = {
+		 0.5f,  0.5f,  0.5f, 1.f,   1.f, 0.f, 0.f, 1.f,
+		 0.5f,  0.5f, -0.5f, 1.f,   0.f, 1.f, 0.f, 1.f,
+		 0.5f, -0.5f,  0.5f, 1.f,   0.f, 0.f, 1.f, 1.f,
+		 0.5f, -0.5f, -0.5f, 1.f,   1.f, 1.f, 0.f, 1.f,
+		-0.5f,  0.5f,  0.5f, 1.f,   1.f, 0.f, 1.f, 1.f,
+		-0.5f,  0.5f, -0.5f, 1.f,   0.f, 1.f, 1.f, 1.f,
+		-0.5f, -0.5f,  0.5f, 1.f,   1.f, 1.f, 1.f, 1.f,
+		-0.5f, -0.5f, -0.5f, 1.f,   1.f, 0.f, 0.f, 1.f,
+	};
+	auto vb = dust::create_vertex_buffer(ctx, dust::buffer_usage_t::immutable, vd, 8, vbd);
+
+	// index-buffer
+	uint16 ibd[] = {
+		4, 5, 7, 7, 6, 4, // -x plane
+		0, 2, 3, 3, 1, 0, // +x plane
+		2, 6, 7, 7, 3, 2, // -y plane
+		0, 1, 5, 5, 4, 0, // +y plane
+		5, 1, 3, 3, 7, 5, // -z plane
+		6, 2, 0, 0, 4, 6, // +z plane
+	};
+	auto ib = dust::create_index_buffer(ctx, dust::buffer_usage_t::immutable, 16, 36, ibd);
+
+
+
+
+	namespace math = atma::math;
+
+	// constant buffer
+	static float t = 0.f;
+	atma::math::matrix4f world_matrix;
+	auto cb = dust::create_constant_buffer(ctx, sizeof(world_matrix), &world_matrix);
+
+	bool running = true;
+
+	window->key_state.on_key_down(fooey::key_t::Alt + fooey::key_t::Enter, [ctx]{
+		ctx->signal_fullscreen_toggle(1);
+	});
+
+	window->key_state.on_key_down(fooey::key_t::Esc, [&running]{
+		running = false;
+	});
+
+	float x = 0.f;
+	float y = 0.f;
+
+	auto position         = math::vector4f{0.f, 0.f, -2.f, 1.f};
+	auto walk_direction   = math::vector4f{0.f, 0.f, 1.f, 0.f};
+	auto strafe_direction = math::vector4f{1.f, 0.f, 0.f, 0.f};
+
+	math::vector4f rotation;
+	float walk_speed = 0.002f;
+
+
+	bool mouse_down = false;
+	int ox = 0, oy = 0;
+	window->on({
+		{"close", [&running](fooey::event_t const&){
+			running = false;
+		}},
+
+		{"mouse-move", [&](fooey::events::mouse_t const& e) {
+			
+			if (mouse_down)
+			{
+				auto dx = e.x() - ox;
+				x -= dx * 0.01f;
+
+				auto dy = e.y() - oy;
+				y -= dy * 0.01f;
+			}
+
+			ox = e.x();
+			oy = e.y();
+		}},
+
+		{"mouse-down.left", [&]{
+			mouse_down = true;
+		}},
+		{"mouse-up.left", [&]{
+			mouse_down = false;
+		}},
+		{"mouse-leave", [&]{
+			mouse_down = false;
+		}}
+	});
+
+	bool W = false, A = false, S = false, D = false;
+	window->key_state.on_key_down(fooey::key_t::W, [&] { 
+		W = true;
+		 });
+	window->key_state.on_key_down(fooey::key_t::A, [&] { A = true; });
+	window->key_state.on_key_down(fooey::key_t::S, [&] { S = true; });
+	window->key_state.on_key_down(fooey::key_t::D, [&] { D = true; });
+
+	window->key_state.on_key_up(fooey::key_t::W, [&] { W = false; });
+	window->key_state.on_key_up(fooey::key_t::A, [&] { A = false; });
+	window->key_state.on_key_up(fooey::key_t::S, [&] { S = false; });
+	window->key_state.on_key_up(fooey::key_t::D, [&] { D = false; });
+
+	//std::chrono::duration<std::chrono::milliseconds> elapsed;
+	std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+	int frames = 0;
+	while (running)
+	{
+		t += 0.1f;
+
+		if (y > atma::math::pi_over_two - 0.1)
+			y = atma::math::pi_over_two - 0.1f;
+		else if (y < -atma::math::pi_over_two + 0.1f)
+			y = -atma::math::pi_over_two + 0.1f;
+
+		if (W) position += walk_direction * walk_speed;
+		if (A) position -= strafe_direction * walk_speed;
+		if (S) position -= walk_direction * walk_speed;
+		if (D) position += strafe_direction * walk_speed;
+
+		//walk_direction = math::point4f(sin(x) * cos(y), sin(y), cos(x) * cos(y));
+		//strafe_direction = math::cross_product(walk_direction, math::vector4f(0.f, 1.f, 0.f, 0.f));
+
+		// camera
+		auto camera = dust::camera_t(
+			//math::look_at(math::point4f(sin(x) * cos(y) * 2.f, sin(y) * 2.f, cos(x) * cos(y) * 2.f), math::point4f(0.f, 0.1f, 0.f), math::vector4f(0.f, 1.f, 0.f, 0.f)),
+			math::look_at(position, position + walk_direction, math::vector4f{0.f, 1.f, 0.f, 0.f}),
+			math::perspective_fov(math::pi_over_two, (float)window->height() / window->width(), 0.03434f, 120.f));
+
+		auto scene = dust::scene_t(ctx, camera);
+
+		world_matrix = math::matrix4f{math::xmmd_identity_r0_ps, math::xmmd_identity_r1_ps, math::xmmd_identity_r2_ps, math::xmmd_identity_r3_ps};
+		scene.signal_res_update(cb, sizeof(world_matrix), &world_matrix);
+		//scene.signal_cs_upload_constant_buffer(1, cb);
+		ctx->signal_vs_upload_constant_buffer(1, cb);
+
+		// do this just to upload the camera :P
+		//ctx->signal_draw_scene(scene);
+		ctx->signal_clear();
+
+		scene.signal_draw(ib, vd, vb, vs, ps);
+		ctx->signal_draw_scene(scene);
+		ctx->signal_block();
+		ctx->signal_present();
+
+#if 0
+		++frames;
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() > 1000)
+		{
+			std::cout << "apparently, width: " << window->width() << ", height: " << window->height() << std::endl;
+			//std::cout << "FPS: " << frames << std::endl;
+			start = std::chrono::high_resolution_clock::now();
+			frames = 0;
+		}
+#endif
+	}
+	ctx->signal_block();
+
 
 #if 0
 	auto sf = shelf::file_t{"../../data/dragon.obj"};
