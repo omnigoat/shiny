0edb114d782a52a38a3eb0c3c5b0592d235db7ba
jona <omnigoat@gmail.com>
2015-06-28 22:15:59 +1000

    fixed up shaders for rendering 3d mesh

--- shaders/gs_normal.hlsl
+++ shaders/gs_normal.hlsl
@@ -4,13 +4,11 @@ struct VSOutput
 	float4 world_position : Position;
 };
 
-
 struct GSOutput
 {
 	float4 position : SV_Position;
-	//float4 world_position : Position;
-	//float3 normal : Normal;
-	float4 color : Color;
+	float4 world_position : Position;
+	float3 normal : Normal;
 };
 
 [maxvertexcount(3)]
@@ -18,13 +16,12 @@ void main(triangle VSOutput input[3], inout TriangleStream<GSOutput> output)
 {
 	float3 normal = cross(input[1].world_position.xyz - input[0].world_position.xyz, input[2].world_position.xyz - input[1].world_position.xyz);
 
-	for (uint i = 0; i < 3; i++)
+	for (uint i = 0; i < 3; ++i)
 	{
 		GSOutput element;
 		element.position = input[i].position;
-		//element.world_position = input[i].world_position;
-		//element.normal = normal;
-		element.color = float4(0.f, 0.f, 0.f, 1.f);
+		element.world_position = input[i].world_position;
+		element.normal = normal;
 		output.Append(element);
 	}
 }
--- shaders/ps_basic.hlsl
+++ shaders/ps_basic.hlsl
@@ -1,16 +1,16 @@
 struct PSInput
 {
 	float4 position : SV_Position;
-	float4 color : Color;
+	float4 world_position : Position;
+	float3 normal : Normal;
 };
 
 float4 main(PSInput input) : SV_Target
 {
-#if 0
 	float3 light = float3(-20.f, 20.f, -10.f);
 	float3 lightdir = normalize(light - input.world_position.xyz);
 
 	float c = max(dot(normalize(input.normal), lightdir), 0.f);
-#endif
-	return float4(input.color.xyzw);
+
+	return float4(c, c, c, 1.f);
 }
