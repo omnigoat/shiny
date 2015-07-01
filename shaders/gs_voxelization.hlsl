3dc1cb64e8a3287aff9a090fae03b8bca88f6b5d
omnigoat <omnigoat@gmail.com>
2015-07-01 10:32:40 +1000

    gs to shader-model 5.0, dilating triangle

--- shaders/gs_voxelization.hlsl
+++ shaders/gs_voxelization.hlsl
@@ -7,8 +7,7 @@ struct VSOutput
 struct GSOutput
 {
 	float4 position : SV_Position;
-	float4 world_position : Position;
-	float3 normal : Normal;
+	nointerpolation float3 normal : Normal;
 };
 
 
@@ -18,7 +17,7 @@ static matrix ortho =
 {
 	float4(1.f, 0.f, 0.f, 0.f),
 	float4(0.f, 1.f, 0.f, 0.f),
-	float4(0.f, 0.f, 0.5f, 0.f),
+	float4(0.f, 0.f, 1.f, 0.f),
 	float4(0.f, 0.f, 0.f, 1.f)
 };
 #endif
@@ -57,9 +56,12 @@ static matrix projs[] =
 [maxvertexcount(3)]
 void main(triangle VSOutput input[3], inout TriangleStream<GSOutput> output)
 {
+	float3 hpixel = 1.4142 / 0.5f;
+
+
 	float3 normal = cross(input[1].world_position.xyz - input[0].world_position.xyz, input[2].world_position.xyz - input[1].world_position.xyz);
 
-	// 
+	// pick projection axis
 	float3 pn = abs(normal);
 	int proj_idx;
 	if (pn.x > pn.y && pn.x > pn.z)
@@ -69,18 +71,39 @@ void main(triangle VSOutput input[3], inout TriangleStream<GSOutput> output)
 	else
 		proj_idx = 2;
 
-	// transformed position
-	float4 position_clipspace[3];
-	for (uint i2 = 0; i2 != 3; ++i2)
-		position_clipspace[i2] = mul(projs[proj_idx], input[i2].world_position);
+	// projected verts
+	float4 v0 = mul(projs[proj_idx], input[0].world_position);
+	float4 v1 = mul(projs[proj_idx], input[1].world_position);
+	float4 v2 = mul(projs[proj_idx], input[2].world_position);
 
+	// edges in homogenous clip-space
+	float3 e01 = v1.xyw - v0.xyw;
+	float3 e12 = v2.xyw - v1.xyw;
+	float3 e20 = v0.xyw - v2.xyw;
 
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.position = input[i].position;
-		element.world_position = input[i].world_position;
-		element.normal = normal;
-		output.Append(element);
-	}
+	// planes in homogenous clip-space
+	float3 p0 = cross(e20, v0.xyw);
+	float3 p1 = cross(e01, v1.xyw);
+	float3 p2 = cross(e12, v2.xyw);
+
+	// dilate planes
+	p0.z -= dot(hpixel.xy, abs(p0.xy));
+	p1.z -= dot(hpixel.xy, abs(p1.xy));
+	p2.z -= dot(hpixel.xy, abs(p2.xy));
+
+	// output triangle
+	GSOutput g0;
+	g0.position = float4(cross(p0, p1), 0.f);
+	g0.normal = normal;
+	output.Append(g0);
+
+	GSOutput g1;
+	g1.position = float4(cross(p1, p2), 0.f);
+	g1.normal = normal;
+	output.Append(g1);
+
+	GSOutput g2;
+	g2.position = float4(cross(p2, p0), 0.f);
+	g2.normal = normal;
+	output.Append(g2);
 }
